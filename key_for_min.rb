3ae6a1e1e5db66a8a8e6a7e63f6d2282c5a8881d
Mark Hernandez <m.hern1415@gmail.com>
2019-05-06 23:58:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,18 +4,18 @@
 
 
 def key_for_min_value(name_hash)
-  current_minimum_val = nil 
-  current_key_assoc_w_min_val = nil 
+  current_minimum_value = nil 
+  current_key_assoc_w_min_value = nil 
    name_hash.each do |key, value|
-    if current_minimum_val == nil 
-      current_minimum_val = value 
-      current_key_assoc_w_min_val = key 
+    if current_minimum_value == nil 
+      current_minimum_value = value 
+      current_key_assoc_w_min_value = key 
     else 
-      if current_minimum_val > value 
-        current_minimum_val = value
-        current_key_assoc_w_min_val = key 
+      if current_minimum_value > value 
+        current_minimum_value = value
+        current_key_assoc_w_min_value = key 
       end
     end
   end 
-  current_key_assoc_w_min_val 
+  current_key_assoc_w_min_value 
 end 
